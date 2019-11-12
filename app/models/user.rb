f133f2b4b1d4e9ed8b23ee9538915b9e8b766841
Steven Parker <sparker81@gmail.com>
2019-11-11 23:18:38 -0500

    Add remember token and save on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
+  before_create :create_remember_token
+  attr_accessor :remember
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, :password_confirmation, length: { minimum: 6 }
   has_secure_password
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def create_remember_token
+    self.remember = new_token
+    update_attribute(:remember_token, digest(remember))
+  end
 end
