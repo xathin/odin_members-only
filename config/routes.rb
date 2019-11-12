3327571376e49c355fd308bf422cabc14f56bedf
Steven Parker <sparker81@gmail.com>
2019-11-11 22:42:47 -0500

    Added basic login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
