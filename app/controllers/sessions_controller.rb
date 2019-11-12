a6da3f9ebca598e7320e57cba642b3551c69a2f3
Steven Parker <sparker81@gmail.com>
2019-11-12 00:09:42 -0500

    Added login methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+    else
+      render 'new'
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.new_remember_token
+    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+  end
 end
